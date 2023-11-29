0102b834ca61a228bada0f0c672fd5b7d5239aa9
Suleyman Laarabi <suleyman.laarabi.dev@gmail.com>
2023-11-30 00:41:30 +0100

    add new ControlPad

---
+++ src/components/views/ControlPad/ControlPadV2.tsx
@@ -0,0 +1,11 @@
+import { Heading } from "@chakra-ui/react";
+
+const ControlPadV2 = () => {
+  return (
+    <>
+      <Heading>Control Pad V2</Heading>
+    </>
+  );
+};
+
+export default ControlPadV2;
