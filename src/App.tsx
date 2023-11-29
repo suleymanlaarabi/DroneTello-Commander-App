56607a33b40436af6942578423a7533338b49d3c
Suleyman Laarabi <suleyman.laarabi.dev@gmail.com>
2023-11-30 00:39:48 +0100

    refactor 2_0

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,12 @@
-import { Button, ChakraProvider, Flex, Heading } from "@chakra-ui/react";
-import {
-  arreter,
-  avancer,
-  bas,
-  demarrer,
-  droite,
-  gauche,
-  haut,
-  reculer,
-} from "./services/api/tello-api";
+import { ChakraProvider } from "@chakra-ui/react";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./services/router";
 
 function App() {
   return (
     <>
       <ChakraProvider>
-        <Flex
-          h={"100vh"}
-          w={"100vw"}
-          alignItems={"center"}
-          justifyContent={"center"}
-          direction={"column"}
-          gap={10}
-        >
-          <Heading>Drone Tello</Heading>
-          <Flex gap={5}>
-            <Button onClick={demarrer}>Démarrer</Button>
-            <Button onClick={arreter}>Arreter</Button>
-          </Flex>
-          <Flex gap={10}>
-            <Flex gap={5} direction={"column"}>
-              <Button onClick={avancer}>Avancer</Button>
-              <Flex gap={5}>
-                <Button onClick={droite}>droite</Button>
-                <Button onClick={gauche}>Gauche</Button>
-              </Flex>
-              <Button onClick={reculer}>reculer</Button>
-            </Flex>
-
-            <Flex gap={5} flexDirection={"column"}>
-              <Button h={"100%"} onClick={haut}>
-                Haut
-              </Button>
-              <Button h={"100%"} onClick={bas}>
-                Bas
-              </Button>
-            </Flex>
-          </Flex>
-        </Flex>
+        <RouterProvider router={router} />
       </ChakraProvider>
     </>
   );
