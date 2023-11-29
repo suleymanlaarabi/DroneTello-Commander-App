16b1008e12d2d0d040c733e53c4f5e30a20055ad
Suleyman Laarabi <suleyman.laarabi.dev@gmail.com>
2023-11-30 00:41:12 +0100

    add new NavBAr

---
+++ src/components/layouts/NavBar.tsx
@@ -0,0 +1,100 @@
+import {
+  Box,
+  Flex,
+  HStack,
+  IconButton,
+  useDisclosure,
+  useColorModeValue,
+  Stack,
+  Heading,
+} from "@chakra-ui/react";
+import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
+import { NavLink } from "react-router-dom";
+
+interface Props {
+  children: React.ReactNode;
+  url: string;
+}
+
+const Links = [
+  {
+    name: "ControlPad V2 (Beta)",
+    path: "control-pad",
+  },
+  {
+    name: "Script",
+    path: "script",
+  },
+  {
+    name: "Create script",
+    path: "create-script",
+  },
+];
+
+const Link = (props: Props) => {
+  const { children } = props;
+
+  return (
+    <Box
+      as={NavLink}
+      px={2}
+      py={1}
+      rounded={"md"}
+      _hover={{
+        textDecoration: "none",
+        bg: useColorModeValue("gray.200", "gray.700"),
+      }}
+      to={props.url}
+    >
+      {children}
+    </Box>
+  );
+};
+
+export default function NavBar() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  return (
+    <>
+      <Box bg={useColorModeValue("gray.100", "gray.900")} px={4}>
+        <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
+          <IconButton
+            size={"md"}
+            icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+            aria-label={"Open Menu"}
+            display={{ md: "none" }}
+            onClick={isOpen ? onClose : onOpen}
+          />
+          <HStack spacing={8} alignItems={"center"}>
+            <Heading as={NavLink} to="/" fontSize={24}>
+              Drone Commander
+            </Heading>
+            <HStack
+              as={"nav"}
+              spacing={4}
+              display={{ base: "none", md: "flex" }}
+            >
+              {Links.map((link) => (
+                <Link url={link.path} key={link.name}>
+                  {link.name}
+                </Link>
+              ))}
+            </HStack>
+          </HStack>
+        </Flex>
+
+        {isOpen ? (
+          <Box pb={4} display={{ md: "none" }}>
+            <Stack as={"nav"} spacing={4}>
+              {Links.map((link) => (
+                <Link url={link.path} key={link.name}>
+                  {link.name}
+                </Link>
+              ))}
+            </Stack>
+          </Box>
+        ) : null}
+      </Box>
+    </>
+  );
+}
