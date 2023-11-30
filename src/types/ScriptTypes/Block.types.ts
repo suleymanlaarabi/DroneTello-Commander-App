7f63e168b0981a68a052333d2f7216bcc178e5e9
Suleyman Laarabi <suleyman.laarabi.dev@gmail.com>
2023-11-30 21:52:02 +0100

    add some BlockName enum

--- src/types/ScriptTypes/Block.types.ts
+++ src/types/ScriptTypes/Block.types.ts
@@ -8,13 +8,17 @@ export enum BlockName {
   Reculer = "reculer",
   Droite = "droite",
   Gauche = "gauche",
+  Haut = "haut",
+  Bas = "bas",
   Arreter = "arreter",
   Demarrer = "demarrer",
+  Emergency = "emergency",
 }
 
 export type Block = {
-  uid?: string;
+  uid: string;
   name: BlockName;
-  time?: number;
-  action: () => BlockResult;
+  time: number;
+  distance: number;
+  action: (distance: number | undefined) => Promise<BlockResult>;
 };
