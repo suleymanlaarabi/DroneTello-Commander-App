988c62051e2244740a37546d316d6387efb1f8de
Suleyman Laarabi <suleyman.laarabi.dev@gmail.com>
2023-11-30 21:49:13 +0100

    component to add an animation

---
+++ src/components/common/anim/Zoom.tsx
@@ -0,0 +1,27 @@
+import { motion } from "framer-motion";
+import { PropsWithChildren } from "react";
+interface props extends PropsWithChildren {
+  id: string;
+}
+
+const Zoom = ({ children, id }: props) => {
+  return (
+    <motion.div
+      layoutId={id}
+      key={id}
+      initial={{
+        scale: 0.7,
+      }}
+      transition={{
+        duration: 0.3,
+      }}
+      animate={{
+        scale: 1,
+      }}
+    >
+      {children}
+    </motion.div>
+  );
+};
+
+export default Zoom;
