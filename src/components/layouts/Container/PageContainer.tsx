d08185275ff6301de3c4fdac1b8261884e32a95e
Suleyman Laarabi <suleyman.laarabi.dev@gmail.com>
2023-11-30 21:49:19 +0100

    use Zoom Animation

--- src/components/layouts/Container/PageContainer.tsx
+++ src/components/layouts/Container/PageContainer.tsx
@@ -1,19 +1,24 @@
 import { Flex } from "@chakra-ui/react";
 import { PropsWithChildren } from "react";
+import { useLocation } from "react-router-dom";
+import Zoom from "../../common/anim/Zoom";
 
 const PageContainer = ({ children }: PropsWithChildren) => {
+  const { pathname } = useLocation();
   return (
-    <Flex
-      h={"100%"}
-      w={"100%"}
-      alignItems={"center"}
-      justifyContent={"center"}
-      direction={"column"}
-      gap={10}
-      p={5}
-    >
-      {children}
-    </Flex>
+    <Zoom id={pathname}>
+      <Flex
+        h={"100%"}
+        w={"100%"}
+        alignItems={"center"}
+        justifyContent={"center"}
+        direction={"column"}
+        gap={10}
+        p={5}
+      >
+        {children}
+      </Flex>
+    </Zoom>
   );
 };
 
