333f861b945647a2b911d70c5a002f98aa9fffb8
Suleyman Laarabi <suleyman.laarabi.dev@gmail.com>
2023-11-30 02:03:39 +0100

    add stop and start enum to BlockName

--- src/types/ScriptTypes/Block.types.ts
+++ src/types/ScriptTypes/Block.types.ts
@@ -8,11 +8,13 @@ export enum BlockName {
   Reculer = "reculer",
   Droite = "droite",
   Gauche = "gauche",
+  Arreter = "arreter",
+  Demarrer = "demarrer",
 }
 
 export type Block = {
   uid?: string;
   name: BlockName;
   time?: number;
-  action?: () => BlockResult;
+  action: () => BlockResult;
 };
