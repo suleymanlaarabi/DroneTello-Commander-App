de1f16b7445cb08d42d8f67282e8257a4d2cca62
Suleyman Laarabi <suleyman.laarabi.dev@gmail.com>
2023-11-30 02:02:48 +0100

    add parsing to add actionFunction

--- src/components/views/Script/Script.tsx
+++ src/components/views/Script/Script.tsx
@@ -1,7 +1,9 @@
-import { Heading, Select } from "@chakra-ui/react";
+import { Button, Flex, Heading, Select } from "@chakra-ui/react";
 
 import { ScriptShema } from "../../../types/ScriptTypes/ScriptShema.types";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import { addActionToScript } from "../../../hooks/reducer/scriptCreation/function";
+import { Block } from "../../../types/ScriptTypes/Block.types";
 
 const Script = () => {
   const [scripts, setScripts] = useState<ScriptShema[]>([]);
@@ -15,18 +17,39 @@ const Script = () => {
             road: [],
           },
         ];
-    console.log(myScripts);
     setScripts(myScripts);
   }, []);
+
+  const scriptSelect = useRef<HTMLSelectElement>(null);
+
+  const handleRunScript = () => {
+    const [script] = scripts.filter(
+      (el) => el.name == scriptSelect.current?.value
+    );
+
+    const parsedScript = addActionToScript(script);
+    console.log(parsedScript);
+    let i;
+    for (i = 0; i < parsedScript.road.length; i++) {
+      const action = parsedScript.road[i] as Block;
+      console.log(action);
+      action.action();
+    }
+  };
 
   return (
     <>
       <Heading>Run your custom script</Heading>
-      <Select>
-        {scripts.map((el, index) => {
-          return <option key={index}>{el.name}</option>;
-        })}
-      </Select>
+      <Flex gap={5}>
+        <Select ref={scriptSelect} w={250}>
+          {scripts.map((el, index) => {
+            return <option key={index}>{el.name}</option>;
+          })}
+        </Select>
+        <Button onClick={handleRunScript} colorScheme="blue">
+          Run
+        </Button>
+      </Flex>
     </>
   );
 };
