70aea5908564b83516a01ae8d2e5e6e1fb13e2ab
Suleyman Laarabi <suleyman.laarabi.dev@gmail.com>
2023-11-30 21:51:18 +0100

    refactor

--- src/services/api/tello-api.tsx
+++ src/services/api/tello-api.tsx
@@ -1,42 +1,61 @@
 import { get } from "../../utils/fetch";
 import config from "../../config.json";
-const value = 10;
-export async function avancer() {
-  console.log("avancer");
-  return get(config.host + "/avancer/" + value);
+import { BlockName } from "../../types/ScriptTypes/Block.types";
+
+const defaultDistance = 35;
+
+const actionWithoutDistance = [
+  BlockName.Demarrer,
+  BlockName.Arreter,
+  BlockName.Emergency,
+];
+
+function moveAction(action: BlockName, distance: number | undefined) {
+  console.log(action);
+  if (actionWithoutDistance.includes(action)) {
+    return get(config.host + "/tello/" + action);
+  } else {
+    return get(
+      config.host +
+        "/tello/" +
+        action +
+        "/" +
+        JSON.stringify(distance ? distance : defaultDistance)
+    );
+  }
 }
 
-export async function reculer() {
-  console.log("reculer");
-  return get(config.host + "/reculer/" + value);
+export async function avancer(value: number | undefined) {
+  return moveAction(BlockName.Avancer, value);
 }
 
-export async function droite() {
-  console.log("droite");
-  return get(config.host + "/droite/" + value);
+export async function reculer(value: number | undefined) {
+  return moveAction(BlockName.Reculer, value);
 }
 
-export async function gauche() {
-  console.log("gauche");
-  return get(config.host + "/gauche/" + value);
+export async function droite(value: number | undefined) {
+  return moveAction(BlockName.Droite, value);
 }
 
-export async function haut() {
-  console.log("haut");
-  return get(config.host + "/haut/" + value);
+export async function gauche(value: number | undefined) {
+  return moveAction(BlockName.Gauche, value);
 }
 
-export async function bas() {
-  console.log("bas");
-  return get(config.host + "/bas/" + value);
+export async function haut(value: number | undefined) {
+  return moveAction(BlockName.Haut, value);
+}
+
+export async function bas(value: number | undefined) {
+  return moveAction(BlockName.Bas, value);
 }
 
 export async function demarrer() {
-  console.log("demarrer");
-  return get(config.host + "/demarrer");
+  return moveAction(BlockName.Demarrer, undefined);
 }
 
 export async function arreter() {
-  console.log("arreter");
-  return get(config.host + "/arreter");
+  return moveAction(BlockName.Arreter, undefined);
+}
+export async function emergency() {
+  return moveAction(BlockName.Emergency, undefined);
 }
