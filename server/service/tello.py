90e3f5a8f07786a412ec46c2f88e38148ebe9a68
Suleyman Laarabi <suleyman.laarabi.dev@gmail.com>
2023-11-30 21:48:35 +0100

    use debug for test tello

--- server/service/tello.py
+++ server/service/tello.py
@@ -1,30 +1,62 @@
 from djitellopy import Tello
-
-tello = Tello()
+import time
 
 def connect():
+    time.sleep(1)
+    print("connect")
+    return
     tello.connect()
 
 def demarrer():
+    time.sleep(1)
+    print("demarrer")
+    return
     tello.takeoff()
 
 def arreter():
+    time.sleep(1)
+    print("arreter")
+    return
     tello.land()
 
-def avancer():
-    tello.move_forward()
+def avancer(value):
+    time.sleep(1)
+    print("avancer")
+    return
+    tello.move_forward(value)
 
-def reculer():
-    tello.move_back()
+def reculer(value):
+    time.sleep(1)
+    print("reculer")
+    return
+    tello.move_back(value)
 
-def droite():
-    tello.move_right()
+def droite(value):
+    time.sleep(1)
+    print("droite")
+    return
+    tello.move_right(value)
 
-def gauche():
-    tello.move_left()
+def gauche(value):
+    time.sleep(1)
+    print("gauche")
+    return
+    tello.move_left(value)
 
-def haut():
-    tello.move_up()
+def haut(value):
+    time.sleep(1)
+    print("haut")
+    return
+    tello.move_up(value)
 
-def bas():
-    tello.move_down()
+def bas(value):
+    time.sleep(1)
+    print("bas")
+    return
+    tello.move_down(value)
+
+def emergency():
+    time.sleep(1)
+    print("emergency")
+    return
+    tello.emergency()
