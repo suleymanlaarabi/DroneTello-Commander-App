3e5b38384f0a9e4cc488d6b83598f36c9b989ccc
Suleyman Laarabi <suleyman.laarabi.dev@gmail.com>
2023-11-30 21:51:25 +0100

    remove unused route

--- src/services/router.tsx
+++ src/services/router.tsx
@@ -2,8 +2,6 @@ import { createBrowserRouter } from "react-router-dom";
 import ControlPad from "../components/views/ControlPad/ControlPad";
 import Root from "../components/Root";
 import Script from "../components/views/Script/Script";
-import ControlPadV2 from "../components/views/ControlPad/ControlPadV2";
-import CreateScript from "../components/views/Script/CreateScript";
 
 export const router = createBrowserRouter([
   {
@@ -17,14 +15,6 @@ export const router = createBrowserRouter([
       {
         path: "script",
         element: <Script />,
-      },
-      {
-        path: "control-pad",
-        element: <ControlPadV2 />,
-      },
-      {
-        path: "create-script",
-        element: <CreateScript />,
       },
     ],
   },
