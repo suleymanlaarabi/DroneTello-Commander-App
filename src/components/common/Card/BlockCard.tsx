d03930c4ff17425eabdf36bb748c20563f69615d
Suleyman Laarabi <suleyman.laarabi.dev@gmail.com>
2023-11-30 21:48:55 +0100

    new BlockCard for script

--- src/components/common/Card/BlockCard.tsx
+++ src/components/common/Card/BlockCard.tsx
@@ -1,7 +1,84 @@
-import { Heading } from "@chakra-ui/react";
+import {
+  Card,
+  CardBody,
+  CardFooter,
+  Divider,
+  Flex,
+  Heading,
+  IconButton,
+} from "@chakra-ui/react";
+import { DeleteIcon, ArrowDownIcon, ArrowUpIcon } from "@chakra-ui/icons";
+import { BlockName } from "../../../types/ScriptTypes/Block.types";
+import Zoom from "../anim/Zoom";
 
-const BlockCard = () => {
-  return <Heading>Card</Heading>;
+type Props = {
+  uid: string;
+  name: BlockName;
+  distance: number;
+  isTop: boolean;
+  isDown: boolean;
+  onMove: (direction: "up" | "down") => void;
+  onDelete: () => void;
+};
+
+const BlockCard = ({
+  uid,
+  name,
+  onDelete,
+  distance,
+  isTop,
+  isDown,
+  onMove,
+}: Props) => {
+  const renderDistance =
+    ![BlockName.Demarrer, BlockName.Arreter].includes(name) &&
+    `: ${distance} cm`;
+  return (
+    <Zoom id={uid}>
+      <Flex>
+        <Card w={300}>
+          <CardBody>
+            <Heading fontSize={24}>
+              {name.toUpperCase()}
+              {renderDistance}
+            </Heading>
+            <Divider />
+          </CardBody>
+          <CardFooter gap={4}>
+            <Flex
+              w="full"
+              justifyContent={"center"}
+              gap={2}
+              direction={"column"}
+            >
+              {!isTop && (
+                <IconButton
+                  onClick={() => onMove("up")}
+                  icon={<ArrowUpIcon />}
+                  aria-label="up"
+                />
+              )}
+              {!isDown && (
+                <IconButton
+                  onClick={() => onMove("down")}
+                  icon={<ArrowDownIcon />}
+                  aria-label="down"
+                />
+              )}
+            </Flex>
+            <IconButton
+              minH={"40px"}
+              height={"full"}
+              onClick={onDelete}
+              aria-label="been"
+              icon={<DeleteIcon />}
+              colorScheme="red"
+            />
+          </CardFooter>
+        </Card>
+      </Flex>
+    </Zoom>
+  );
 };
 
 export default BlockCard;
