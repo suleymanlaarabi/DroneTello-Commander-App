753ef9f109ebd813a4a14cffed51cf4e66189cb7
Suleyman Laarabi <suleyman.laarabi.dev@gmail.com>
2023-11-30 21:49:04 +0100

    improve modal blockCard

--- src/components/common/Modal/CreateBlockModal.tsx
+++ src/components/common/Modal/CreateBlockModal.tsx
@@ -1,5 +1,7 @@
 import {
   Button,
+  Divider,
+  Text,
   Modal,
   ModalBody,
   ModalCloseButton,
@@ -7,16 +9,23 @@ import {
   ModalFooter,
   ModalHeader,
   ModalOverlay,
-  ModalProps,
   Select,
+  Slider,
+  SliderFilledTrack,
+  SliderThumb,
+  SliderTrack,
+  Tooltip,
+  Flex,
+  IconButton,
 } from "@chakra-ui/react";
 import { BlockName } from "../../../types/ScriptTypes/Block.types";
-import { useRef } from "react";
+import { useState } from "react";
+import { CreateBlockActionTypes } from "../../../types/ModalTypes";
+import { ArrowBackIcon } from "@chakra-ui/icons";
 
-interface CreateBlockModalProps extends ModalProps {
+interface CreateBlockModalProps extends CreateBlockActionTypes {
   isOpen: boolean;
   onClose: () => void;
-  onConfirm: (name: BlockName) => void;
 }
 
 export function CreateBlockModal({
@@ -25,33 +34,97 @@ export function CreateBlockModal({
   onConfirm,
 }: CreateBlockModalProps) {
   const handleSave = () => {
-    if (selectRef.current) {
-      onConfirm(selectRef.current.value as BlockName);
-    }
+    onConfirm(select, time, distance);
     onClose();
   };
-  const selectRef = useRef<HTMLSelectElement>(null);
+  const [showTooltip, setShowTooltip] = useState(false);
+  const [time, setTime] = useState(0);
+
+  const [distance, setDistance] = useState(20);
+
+  const [select, setSelect] = useState<BlockName>(BlockName.Demarrer);
+
   return (
     <>
       <Modal isCentered isOpen={isOpen} onClose={onClose}>
         <ModalOverlay />
         <ModalContent>
-          <ModalHeader>Create your account</ModalHeader>
+          <ModalHeader>New Block</ModalHeader>
           <ModalCloseButton />
           <ModalBody pb={6}>
-            <Select ref={selectRef} placeholder="Select action">
-              <option value={BlockName.Demarrer}>Demarrer</option>
-              <option value={BlockName.Arreter}>Arreter</option>
-              <option value={BlockName.Avancer}>Avancer</option>
-              <option value={BlockName.Reculer}>Reculer</option>
-              <option value={BlockName.Droite}>Droite</option>
-              <option value={BlockName.Gauche}>Gauche</option>
-            </Select>
+            <Flex>
+              {[BlockName.Demarrer, BlockName.Arreter].includes(select) ? (
+                <Select
+                  onChange={(e) => setSelect(e.target.value as BlockName)}
+                  value={select}
+                  placeholder="Select action"
+                >
+                  <option value={BlockName.Demarrer}>Demarrer</option>
+                  <option value={BlockName.Arreter}>Arreter</option>
+                  <option value={BlockName.Avancer}>Avancer</option>
+                  <option value={BlockName.Reculer}>Reculer</option>
+                  <option value={BlockName.Droite}>Droite</option>
+                  <option value={BlockName.Gauche}>Gauche</option>
+                </Select>
+              ) : (
+                <Flex gap={3} direction={"column"} w={"100%"}>
+                  <Flex alignItems={"center"} gap={4}>
+                    <IconButton
+                      w="45px"
+                      h="45px"
+                      icon={<ArrowBackIcon />}
+                      aria-label="back"
+                      onClick={() => setSelect(BlockName.Demarrer)}
+                    />
+                    <Text>Distance: {distance}cm</Text>
+                  </Flex>
+                  <Slider
+                    min={20}
+                    max={500}
+                    step={10}
+                    value={distance}
+                    onChange={(e) => setDistance(e)}
+                  >
+                    <SliderTrack>
+                      <SliderFilledTrack />
+                    </SliderTrack>
+
+                    <SliderThumb />
+                  </Slider>
+                </Flex>
+              )}
+            </Flex>
+
+            <Divider my={5} />
+            <Text>Time before second action: {time}ms</Text>
+            <Slider
+              min={0}
+              max={5000}
+              step={100}
+              onMouseEnter={() => setShowTooltip(true)}
+              onMouseLeave={() => setShowTooltip(false)}
+              value={time}
+              onChange={(e) => setTime(e)}
+            >
+              <SliderTrack>
+                <SliderFilledTrack />
+              </SliderTrack>
+              <Tooltip
+                hasArrow
+                bg="blue.500"
+                color="white"
+                placement="top"
+                isOpen={showTooltip}
+                label={`${time}ms`}
+              >
+                <SliderThumb />
+              </Tooltip>
+            </Slider>
           </ModalBody>
 
           <ModalFooter>
             <Button onClick={handleSave} colorScheme="blue" mr={3}>
-              Save
+              Add
             </Button>
             <Button onClick={onClose}>Cancel</Button>
           </ModalFooter>
