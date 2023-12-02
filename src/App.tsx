0055980b783aafd5a9a8ad60b232b89917472285
Suleyman Laarabi <suleyman.laarabi.dev@gmail.com>
2023-12-02 02:12:29 +0100

    add DroneContext 3d view

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./services/router";
+import Drone3DContextProvider from "./context/Drone3DContextProvider";
 
 function App() {
   return (
     <>
       <ChakraProvider>
-        <RouterProvider router={router} />
+        <Drone3DContextProvider>
+          <RouterProvider router={router} />
+        </Drone3DContextProvider>
       </ChakraProvider>
     </>
   );
